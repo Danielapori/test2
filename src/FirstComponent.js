6522afc7783179817751b182df4a473180e38d7a
daniels <aporidaniel235@gmail.com>
2020-10-10 14:14:25 +0000

    FirstComoponent

---
+++ src/FirstComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FirstComponent () {
+    return (
+        <>
+            <p>This is the my first react app</p>
+        </>
+    );
+}
+
+export default FirstComponent;
